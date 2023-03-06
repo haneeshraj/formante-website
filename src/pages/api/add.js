bfd0c77282b85f91f39b629b34728671cf911e26
Haneesh Raj <haneeshraj.banisetti@gmail.com>
2023-03-07 01:51:37 +0530

    testing-Formante

---
+++ src/pages/api/add.js
@@ -0,0 +1,9 @@
+import { addToSheets } from "@/utils/sheets";
+
+export default function handler(req, res) {
+  const { name, email, number, subject, message } = req.body;
+  if (req.method === "POST") {
+    addToSheets(name, email, number, subject, message);
+    res.json({ message: "success" });
+  }
+}
---
+++ src/utils/sheets.js
@@ -0,0 +1,28 @@
+import { google } from "googleapis";
+
+const addToSheets = async (name, email, number, subject, message) => {
+  const auth = new google.auth.GoogleAuth({
+    keyFile: "./src/utils/cred.json",
+    scopes: "https://www.googleapis.com/auth/spreadsheets",
+  });
+  const client = await auth.getClient();
+  const googleSheets = google.sheets({ version: "v4", auth: client });
+
+  const spreadsheetId = process.env.EXCEL_SHEET_ID;
+  const metaData = await googleSheets.spreadsheets.get({
+    auth,
+    spreadsheetId,
+  });
+  googleSheets.spreadsheets.values.append({
+    auth,
+    spreadsheetId,
+    range: "messages!A:E",
+    valueInputOption: "USER_ENTERED",
+    resource: {
+      values: [[name, email, number, subject, message]],
+    },
+  });
+  console.log("Your message is sent, " + name + "!");
+};
+
+export { addToSheets };
